9ee50c9a2720e88f8aee0c1bd2ee65cfa16f570f
simonlally <simonlallydev@gmail.com>
2019-09-23 20:32:33 -0400

    add first and add last node functions

---
+++ linkedlist/Linkedlist.js
@@ -0,0 +1,43 @@
+// Simple implemenation for a Singly Linked List in JavaScript
+
+class Node {
+  constructor(data, next = null) {
+    (this.data = data), (this.next = next);
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    let node = new Node(data);
+    node.next = this.head;
+    this.head = node;
+    return this.head;
+  }
+
+  insertLast(data) {
+    let node = new Node(data);
+
+    // check if list is empty
+    if (!this.head) {
+      this.head = node;
+      return this.head;
+    }
+
+    let tail = this.head;
+    while (tail.next !== null) {
+      tail = tail.next;
+    }
+    tail.next = node;
+    return this.head;
+  }
+}
+
+let linkedList = new LinkedList();
+
+linkedList.insertFirst(1);
+linkedList.insertLast(2);
+console.log(linkedList);
