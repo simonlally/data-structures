8831850bea0224a0db3b74cbb14860f7a88b3626
simonlally <simonlallydev@gmail.com>
2019-10-03 11:36:47 -0400

    added index file as main entry point

---
+++ linkedlist/index.js
@@ -0,0 +1,8 @@
+const LinkedList = require("./Linkedlist");
+
+var linkedList = new LinkedList();
+linkedList.insertFirst("y");
+linkedList.insertLast("zzz");
+linkedList.insertAt(0, "p");
+
+console.log(linkedList);
